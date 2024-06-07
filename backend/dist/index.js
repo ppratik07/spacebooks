fda398aa40efe7f73861d41f365c88a223bd130c
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-07 22:15:28 +0530

    dist

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -15,11 +15,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const client_1 = require("@prisma/client");
 const Register_1 = require("./zod/Register");
+const Signin_1 = require("./zod/Signin");
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
 const jwt = require("jsonwebtoken");
 const prisma = new client_1.PrismaClient();
+//Endpoints
 app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { success } = Register_1.register.safeParse(req.body);
     if (!success) {
@@ -37,6 +39,30 @@ app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     const token = yield jwt.sign({ id: user.id }, process.env.JWT_SECRET);
     return res.status(200).json({
         message: "User created successfully",
+        jwt: token,
+    });
+}));
+app.post('/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { success } = Signin_1.signin.safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            message: "Error signing in. Please check email and password!"
+        });
+    }
+    const getUser = yield prisma.user.findUnique({
+        where: {
+            username: req.body.username,
+            password: req.body.password,
+        },
+    });
+    if (!getUser) {
+        res.status(403).send({
+            message: "user not found/Incorrect credentials"
+        });
+    }
+    const token = yield jwt.sign({ id: getUser === null || getUser === void 0 ? void 0 : getUser.id }, process.env.JWT_SECRET);
+    return res.status(200).json({
+        message: "User signied in successfully",
         jwt: token,
     });
 }));
