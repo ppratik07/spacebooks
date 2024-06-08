cbc7c84a3675b8f88df5dd54d2cfa228e5921ee1
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-09 04:12:57 +0530

    reset password endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { PrismaClient, Prisma } from "@prisma/client";
 import { register } from "./zod/Register";
 import { signin } from "./zod/Signin";
 import authMiddleware from "./middlewares/auth";
+import { resetpassword } from "./zod/ResetPassword";
 const app = express();
 const PORT = 3000;
 
@@ -58,6 +59,42 @@ app.post("/signin", async (req, res) => {
   });
 });
 
+app.get("/reset-password", async(req, res) => {
+  const { success } = resetpassword.safeParse(req.body);
+  const user = await prisma.user.findUnique({ where: { username : req.body.username } });
+    if (!user) {
+      return res.status(400).send('User not found');
+    }
+    //send email
+  if (!success) {
+    res.status(411).json({
+      message: "Please check the correct inputs",
+    });
+  }
+  try {
+    const resetToken = jwt.sign(
+      { userId: user.id  },
+      process.env.RESET_PASSWORD_SECRET as string,
+      { expiresIn: "1h" }
+    );
+    res.send("Password reset email sent");
+  } catch (error) {
+    res.status(500).send('Error sending password reset email');
+  }
+});
+
+app.post('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+    const { newPassword } = req.body;
+    try {
+      const decoded = jwt.verify(token, process.env.RESET_PASSWORD_SECRET as string) as { userId: number };
+      await prisma.user.update({ where: { id: decoded.userId }, data: { password: newPassword } });
+      res.send('Password reset successful');
+    } catch (error) {
+      res.status(500).send('Error resetting password');
+    }
+  });
+
 app.get("/profile", authMiddleware, async (req, res) => {
   try {
     const user = await prisma.user.findUnique({
@@ -71,5 +108,5 @@ app.get("/profile", authMiddleware, async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`app listening on ${PORT}`);
+  console.log(`app listening on $ {PORT}`);
 });
