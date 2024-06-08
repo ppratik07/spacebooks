e94c75ffdb1a69e3be4634a05d58da92d6714a9e
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-08 21:43:51 +0530

    created authentication middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+const { JWTSECRET }: any = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(403).json({});
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decode = jwt.verify(token, JWTSECRET);
+    (req as any).user = decode;
+  } catch (error) {
+    res.status(400).send("Invalid token.");
+  }
+};
+
+export default authMiddleware;
