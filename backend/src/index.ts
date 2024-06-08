9f95ade29deff2c75b0c27c135908678bfacf5e8
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-08 21:44:21 +0530

    Created signin endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { register } from "./zod/Register";
 import { signin } from "./zod/Signin";
+import authMiddleware from "./middlewares/auth";
 const app = express();
 const PORT = 3000;
 
@@ -32,30 +33,42 @@ app.post("/signup", async (req, res) => {
   });
 });
 
-app.post('/signin',async(req,res)=>{
-    const {success} = signin.safeParse(req.body);
-    if(!success){
-        res.status(411).json({
-            message : "Error signing in. Please check email and password!"
-        });
-    }
-    const getUser = await prisma.user.findUnique({
-        where: {
-          username: req.body.username,
-          password: req.body.password,
-        },
-      });
-      if(!getUser){
-            res.status(403).send({
-                message : "user not found/Incorrect credentials"
-            })
-      }
-      const token = await jwt.sign({id : getUser?.id},process.env.JWT_SECRET);
-      return res.status(200).json({
-        message: "User signied in successfully",
-        jwt: token,
-      });
-})
+app.post("/signin", async (req, res) => {
+  const { success } = signin.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error signing in. Please check email and password!",
+    });
+  }
+  const getUser = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+      password: req.body.password,
+    },
+  });
+  if (!getUser) {
+    res.status(403).send({
+      message: "user not found/Incorrect credentials",
+    });
+  }
+  const token = await jwt.sign({ id: getUser?.id }, process.env.JWT_SECRET);
+  return res.status(200).json({
+    message: "User signied in successfully",
+    jwt: token,
+  });
+});
+
+app.get("/profile", authMiddleware, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: (req as any).user.userId },
+      select: { username: true },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).send("Error fetching user profile");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
