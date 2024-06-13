b8be96b9639e7e32534eafab8b8b68288d044ba9
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-14 00:25:37 +0530

    compiled auth js

---
+++ backend/dist/middlewares/auth.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { JWTSECRET } = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(403).json({});
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decode = jwt.verify(token, JWTSECRET);
+        req.user = decode;
+    }
+    catch (error) {
+        res.status(400).send("Invalid token.");
+    }
+};
+exports.default = authMiddleware;
