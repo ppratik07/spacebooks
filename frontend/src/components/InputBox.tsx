e1a95f5fa08b2c1e0de7d1e8a990442e38898c25
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-13 18:18:29 +0530

    added value as props and parameter

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -4,15 +4,16 @@ interface InputProps {
     label: string;
     id: string;
     type: string;
+    value : string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputBox: React.FC<InputProps> = ({ label, id, type, onChange }) => {
+const InputBox: React.FC<InputProps> = ({ label, id, type, value, onChange }) => {
     return (
         <div>
             <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
             <div className="mt-2">
-                <input id={id} type={type} required
+                <input id={id} type={type} value={value} required
                     className="block w-full rounded-md border-0 py-1.5
                  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
                  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={onChange} />
