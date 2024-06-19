438d5575853891dadc3f8fff290b236e1e448aa8
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-19 21:40:47 +0530

    Created Button

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,13 @@
+
+interface ButtonProps{
+    label : string;
+    type : string;
+    onClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function Button({label, onClick} : ButtonProps) {
+    return <button onClick={onClick} type="button" className="flex w-full justify-center rounded-md
+    bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+     hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+      focus-visible:outline-indigo-600">{label}</button>
+}
