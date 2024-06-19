e1ee19a969edd4d2018e605b432c65ee43863c09
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-19 21:40:59 +0530

    Added in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 function App() {
     // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     // const [user, setUser] = useState<{ id: string; name: string } | null>(null);
@@ -16,6 +17,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login/>} />
+                    <Route path="/register" element={<Register/>} />
                 </Routes>
             </BrowserRouter>
             {/* {isAuthenticated && <div>Welcome, {user?.name}!</div>} */}
