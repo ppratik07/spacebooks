dc99ee18262bcfd995afc08fff18a0baf9cbc614
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-19 20:42:19 +0530

    updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-    const [user, setUser] = useState<{ id: string; name: string } | null>(null);
+    // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    // const [user, setUser] = useState<{ id: string; name: string } | null>(null);
 
-    const handleSignIn = (token: string, user: { id: string; name: string }) => {
+    // const handleSignIn = (token: string, user: { id: string; name: string }) => {
   
-        localStorage.setItem('authToken', token);
-        setUser(user);
-        setIsAuthenticated(true);
-    };
+    //     localStorage.setItem('authToken', token);
+    //     setUser(user);
+    //     setIsAuthenticated(true);
+    // };
 
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/login" element={<Login onSignIn={handleSignIn} />} />
+                    <Route path="/login" element={<Login/>} />
                 </Routes>
             </BrowserRouter>
-            {isAuthenticated && <div>Welcome, {user?.name}!</div>}
+            {/* {isAuthenticated && <div>Welcome, {user?.name}!</div>} */}
         </div>
     );
 }
