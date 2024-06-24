d12d3999908d459a3c010812208f3061a9367614
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-25 00:53:34 +0530

    added button

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,20 +1,25 @@
 import { useState } from "react"
 import { Heading } from "../components/Heading"
 import InputBox from "../components/InputBox"
+import { Button } from "../components/Button";
 
 export const ResetPassword = () => {
 
-const [EmailSent, SetEmailSent] = useState<string>(" ");
+    const [EmailSent, SetEmailSent] = useState<string>(" ");
 
-    return <div className="">
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <Heading label={"Reset your account"} />
-            </div>
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <InputBox label={"Email address"} id={"email"} type={"email"} value={EmailSent} onChange={(e)=>{
-                    SetEmailSent(e.target.value);
-                }} />
+    return <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <Heading label={"Reset your account"} />
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <InputBox label={"Email address"} id={"email"} type={"email"} value={EmailSent} onChange={(e) => {
+                SetEmailSent(e.target.value);
+            }} />
+
+            <div>
+                <Button onClick={async () => {
+
+                }} type={"submit"} label={"Submit"} />
             </div>
         </div>
     </div>
