0b777e8906ad0845adb1c1f23b783963752cf576
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-24 20:41:55 +0530

    added reset password route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { ResetPassword } from './pages/ResetPassword';
 function App() {
     // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     // const [user, setUser] = useState<{ id: string; name: string } | null>(null);
 
     // const handleSignIn = (token: string, user: { id: string; name: string }) => {
-  
+
     //     localStorage.setItem('authToken', token);
     //     setUser(user);
     //     setIsAuthenticated(true);
@@ -16,8 +17,9 @@ function App() {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/login" element={<Login/>} />
-                    <Route path="/register" element={<Register/>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path='/resetpassword' element={<ResetPassword />} />
                 </Routes>
             </BrowserRouter>
             {/* {isAuthenticated && <div>Welcome, {user?.name}!</div>} */}
