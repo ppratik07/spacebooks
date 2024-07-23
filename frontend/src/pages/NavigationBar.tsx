5778c54547685de40777b035391ab9f4075ffeaa
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-07-24 01:41:50 +0530

    updaating nav

--- frontend/src/pages/NavigationBar.tsx
+++ frontend/src/pages/NavigationBar.tsx
@@ -4,7 +4,7 @@ import { Logout } from './Logout';
 export const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <div className="text-white text-lg">Book Your Seat</div>
+      <div className="text-white text-lg"><a href='/login'>Book My Seat</a></div>
       <Logout />
     </nav>
   );
