7ccb72a76c0c6b8c6593f441aee5493bff771b6a
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-10 21:49:46 +0530

    Updating resetpassword

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -2,10 +2,11 @@ import { useState } from "react"
 import { Heading } from "../components/Heading"
 import InputBox from "../components/InputBox"
 import { Button } from "../components/Button";
+import axios from "axios";
 
 export const ResetPassword = () => {
 
-    const [EmailSent, SetEmailSent] = useState<string>(" ");
+    const [EmailSent, SetEmailSent] = useState<string>("");
 
     return <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -18,7 +19,14 @@ export const ResetPassword = () => {
 
             <div className="py-2">
                 <Button onClick={async () => {
-
+                    const response = await axios.post("http://localhost:3000/api/request-otp",{
+                        EmailSent
+                    });
+                    if(response.status == 200){
+                        alert("OTP has been sent to your registered email address");
+                    }else{
+                        alert("An error occured while sending OTP");
+                    }
                 }} type={"submit"} label={"Submit"} />
             </div>
         </div>
