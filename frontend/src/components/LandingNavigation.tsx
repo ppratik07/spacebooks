f9d00e0f027b5188d23666d7e6847fd22b8ed487
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-18 21:44:06 +0530

    updating navigation bar

--- frontend/src/components/LandingNavigation.tsx
+++ frontend/src/components/LandingNavigation.tsx
@@ -5,7 +5,7 @@ export const LandingNavigation = () => {
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Book Your Seat</span>
                     </a>
                     <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
