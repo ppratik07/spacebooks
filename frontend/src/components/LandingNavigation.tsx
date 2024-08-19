3c981eb23b75f8122ad2ea6d4da9f6b6b30e5e52
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-19 21:38:48 +0530

    changing links

--- frontend/src/components/LandingNavigation.tsx
+++ frontend/src/components/LandingNavigation.tsx
@@ -4,7 +4,7 @@ export const LandingNavigation = () => {
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://www.zoho.com/sites/zweb/images/creator/appdeck/seat-booking.svg" className="h-8" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Book Your Seat</span>
                     </a>
