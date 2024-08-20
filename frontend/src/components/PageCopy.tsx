e40c78c1840b4c08a2de368056bbc914c872814b
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-20 22:20:50 +0530

    Added PageCopy

---
+++ frontend/src/components/PageCopy.tsx
@@ -0,0 +1,9 @@
+export const PageCopy = () => {
+    return (
+        <div>
+            <div className="ml-60 mt-32 text-4xl font-bold">
+                <h1>Online Seat Booking</h1>
+            </div>
+        </div>
+    )
+}
