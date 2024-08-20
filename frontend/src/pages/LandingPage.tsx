2c44cd8bbc3a57728e0c28b4cf2e6f2396652d15
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-20 22:21:03 +0530

    Updating the landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { LandingNavigation } from "../components/LandingNavigation"
+import { PageCopy } from "../components/PageCopy"
 
 export const LandingPage = ()=>{
     return(
         <div>
             <LandingNavigation/>
+            <PageCopy/>
         </div>
     )
 }
