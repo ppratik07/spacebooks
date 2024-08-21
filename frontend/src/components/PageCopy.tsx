e6cfcf35d330d889fb9ee283107030e9ec92c8f6
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-21 21:49:44 +0530

    Updating pagecopy

--- frontend/src/components/PageCopy.tsx
+++ frontend/src/components/PageCopy.tsx
@@ -1,8 +1,12 @@
 export const PageCopy = () => {
     return (
         <div>
-            <div className="ml-60 mt-32 text-4xl font-bold">
+            <div className="mt-32 ml-36">
+                <img src="https://www.zoho.com/sites/zweb/images/creator/appdeck/seat-booking.svg" alt="bus" />
+            </div>
+            <div className="ml-72 text-4xl -mt-24 font-bold">
                 <h1>Online Seat Booking</h1>
+                <div className="font-light text-2xl ml-1">Reservations made easy</div>
             </div>
         </div>
     )
