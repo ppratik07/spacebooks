1f4c73a47fb864ebb8603f6e9951f242ecb1e828
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-21 21:49:54 +0530

    Landing page

--- frontend/src/components/LandingNavigation.tsx
+++ frontend/src/components/LandingNavigation.tsx
@@ -5,7 +5,7 @@ export const LandingNavigation = () => {
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://www.zoho.com/sites/zweb/images/creator/appdeck/seat-booking.svg" className="h-8" alt="Flowbite Logo" />
+                        <img src="https://www.zoho.com/sites/zweb/images/creator/appdeck/seat-booking.svg" className="h-8" alt="bus Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Book Your Seat</span>
                     </a>
                     <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
