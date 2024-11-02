e74d483e8d9e4ca796467b9608612a0c0902f8a7
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-11-03 00:37:40 +0530

    Created Mainpage

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import { Dropdown } from "../components/Dropdown"
+import MapComponent from "../components/MapComponent"
+
+export const MainPage = () => {
+    return <div className="pt-16">
+        <Dropdown/>
+        <MapComponent />
+    </div>
+}
