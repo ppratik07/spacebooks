3f4b066c793cfac2cee1228d020d39d16341374b
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-11-29 02:01:25 +0530

    Updated LandingPage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,15 +1,19 @@
+import { BookingFeatures } from "../components/BookingFeatures"
+import FooterBanner from "../components/FooterBanner"
+import { FooterSection } from "../components/FooterSection"
 import { HeroSection } from "../components/HeroSection"
 import { MainHeadline } from "../components/MainHeadline"
+import Testimonials from "../components/Testimonials"
 
 export const LandingPage = ()=>{
     return(
          <div>
-            {/* <LandingNavigation/> */}
-            {/* <PageCopy/> */}
-            {/* <Navigation/> */}
-            {/* <Header/> */}
             <HeroSection/>
             <MainHeadline/>
+            <BookingFeatures/>
+            <Testimonials/>
+            <FooterBanner/>
+            <FooterSection/>
         </div>
     )
 }
