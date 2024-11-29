0da153424e67f1cb1e514cc6eca2913533ee09b2
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-11-30 02:22:23 +0530

    onclick button

--- frontend/src/components/FooterBanner.tsx
+++ frontend/src/components/FooterBanner.tsx
@@ -1,4 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 const FooterBanner = () => {
+    const navigate = useNavigate();
+    const handleClick : () => void = ()=>{    
+        navigate('/login');
+    }    
   return (
     <div className="bg-blue-600 text-white py-16 text-center">
       <h1 className="text-4xl md:text-5xl font-bold mb-4">
@@ -8,7 +14,7 @@ const FooterBanner = () => {
         Say goodbye to the chaos of space allocation and <br />
         welcome a new era of workplace harmony.
       </p>
-      <button className="bg-white text-blue-600 font-semibold py-3 px-6 rounded-md shadow-md hover:bg-blue-50 transition duration-300">
+      <button onClick={handleClick} className="bg-white text-blue-600 font-semibold py-3 px-6 rounded-md shadow-md hover:bg-blue-50 transition duration-300">
         Get Started Free
       </button>
     </div>
