02fa80a132796ebf0766366941aab061eca2d1db
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-11-30 02:33:57 +0530

    Updated text

--- frontend/src/components/FooterSection.tsx
+++ frontend/src/components/FooterSection.tsx
@@ -12,24 +12,24 @@ export const FooterSection = () => {
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="/" className="hover:underline">SpaceBooks</a>
+                                        <a href="/" className="hover:underline">About Us</a>
                                     </li>
                                     <li>
-                                        <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                                        <a href="https://tailwindcss.com/" className="hover:underline">Careers</a>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Help Center</a>
                                     </li>
                                     <li>
-                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Prices</a>
                                     </li>
                                 </ul>
                             </div>
