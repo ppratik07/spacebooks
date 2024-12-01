50b10188f147030eeac0ba9e70c1e63314586598
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-12-01 14:00:43 +0530

    Logoheading page

---
+++ frontend/src/components/LogoHeading.tsx
@@ -0,0 +1,9 @@
+export const LogoHeader = () => {
+    return (
+        <div>
+            <header className="flex justify-between items-center px-10 py-5">
+                <h1 className="text-xl font-bold">SpaceBooks</h1>
+            </header>
+        </div>
+    );
+}
