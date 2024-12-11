b0dd16fc96c24a3e1563159115061786d15e7556
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-12-11 21:37:08 +0530

    Updating MyBookingsPage

--- frontend/src/components/LeftNavigation/MyBookings.tsx
+++ frontend/src/components/LeftNavigation/MyBookings.tsx
@@ -1,7 +1,8 @@
 export const MyBookings = () =>{
+    const userName = localStorage.getItem('name');
     return(
         <div>
-            
+            <p>Hi, {userName} ! You don't have any active bookings.</p>
         </div>
     )
 }
