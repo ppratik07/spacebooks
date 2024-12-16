890a5fca4cffe6ffa4ae513dd18bf3e684dfe166
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-12-16 21:57:03 +0530

    Fixing CSS

--- frontend/src/components/ui/Confirmation.tsx
+++ frontend/src/components/ui/Confirmation.tsx
@@ -1,7 +1,7 @@
 export const ConfirmationPage: React.FC = () => {
     return (
         <div> 
-            <div className="flex items-center justify-center min-h-screen bg-gray-500">
+            <div className="flex items-center justify-center">
                 <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg text-center">
                     {/* Success Icon */}
                     <div className="flex items-center justify-center mb-6">
