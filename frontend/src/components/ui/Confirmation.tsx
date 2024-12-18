9f9f7e4218c51b07294317964ec25a78876bf222
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-12-18 22:05:01 +0530

    adding types

--- frontend/src/components/ui/Confirmation.tsx
+++ frontend/src/components/ui/Confirmation.tsx
@@ -1,9 +1,16 @@
-export const ConfirmationPage: React.FC = () => {
+interface ConfirmationProps {
+    name: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+  }
+
+export const ConfirmationPage: React.FC<ConfirmationProps> = ({name,date,startTime,endTime,}) => {
     return (
-        <div> 
+        <div>
             <div className="flex items-center justify-center">
                 <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg text-center">
-                  
+
                     <div className="flex items-center justify-center mb-6">
                         <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center">
                             <svg
@@ -23,17 +30,19 @@ export const ConfirmationPage: React.FC = () => {
                         </div>
                     </div>
 
-             
+
                     <h2 className="text-2xl font-semibold text-gray-900 mb-2">
-                       Your Desk is booked !
+                        Your Desk is booked !
                     </h2>
 
-                
+
                     <p className="text-gray-500 mb-6">
-                       You'll receive a confirmation email with your booking details !
+                        You'll receive a confirmation email with your booking details !
                     </p>
 
-           
+                    <p className="text-gray-500 mb-6">
+                        {date},{startTime}-{endTime}
+                    </p>
                     <button className="w-full py-3 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300">
                         Go back to dashboard
                     </button>
