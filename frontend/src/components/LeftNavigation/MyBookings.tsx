82de65e07e42ad942e5c07adf1f2592f0b4f6d89
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-12-23 21:13:28 +0530

    deleting bookings code

--- frontend/src/components/LeftNavigation/MyBookings.tsx
+++ frontend/src/components/LeftNavigation/MyBookings.tsx
@@ -42,9 +42,11 @@ export const MyBookings = () => {
             });
             if (response.ok) {
                 setBookings((prev) => prev.filter((booking) => booking.id !== id))
+                alert("deleted");
             }
         } catch (error) {
             console.error("Error deleting booking:", error);
+            alert("Error deleting");
         }
     };
     if (loading) {
@@ -84,12 +86,12 @@ export const MyBookings = () => {
                                     </p>
                                 </div>
                                 <div className="flex mt-4 space-x-2">
-                                    <button onClick={()=>deleteBooking(booking.id)}
+                                    <button 
                                         className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
                                     >
                                         Edit
                                     </button>
-                                    <button
+                                    <button onClick={()=>deleteBooking(booking.id)}
                                         className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
                                     >
                                         Delete
