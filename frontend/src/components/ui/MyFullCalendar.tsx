2420faf6caf8876f2eae7e94aff04993ef71d667
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-01-31 17:39:13 +0530

    updating text

--- frontend/src/components/ui/MyFullCalendar.tsx
+++ frontend/src/components/ui/MyFullCalendar.tsx
@@ -41,7 +41,7 @@ const MyFullCalendar = () => {
 
   return (
     <div>
-      <h1 className="text-xl font-bold mb-4">My Full Calendar</h1>
+      <h1 className="text-xl font-bold mb-4">See your bookings : </h1>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
