859fef71d575db050288142cfd79b8a0d8e13285
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-02-09 23:17:15 +0530

    Adding sessions

--- frontend/src/pages/HamburgerMenu.tsx
+++ frontend/src/pages/HamburgerMenu.tsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 import SideNavigation from "../components/LeftNavigation/SideNavigation";
 import { BookingProvider } from "../components/Context/BookingContext";
+import { useNavigate } from "react-router-dom";
 
 export const HamburgerMenu = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const expiration = localStorage.getItem("token_expiration");
+        if (!token || (expiration && Date.now() > Number(expiration))) {
+            alert("Session terminated. Please login again.");
+            localStorage.clear();
+            navigate("/login");
+        }
+    }, [])
     return (
         <div>
             <BookingProvider>
