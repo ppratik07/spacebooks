0281cb33331ba3584b1f5fe47723fb699a81a988
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-02-11 22:43:34 +0530

    unsued commits

--- frontend/src/pages/_MainPage.tsx
+++ frontend/src/pages/_MainPage.tsx
@@ -1,9 +1,7 @@
 import { Dropdown } from "../components/ui/Dropdown"
-import MapComponent from "../components/ui/_MapComponent"
 
 export const MainPage = () => {
     return <div className="pt-16">
         <Dropdown/>
-        <MapComponent />
     </div>
 }
