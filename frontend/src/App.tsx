614012b71b543ce28ef36024ac037c5ff7584142
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-02-15 15:29:19 +0530

    Removing unused codes from app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,27 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { ResetPassword } from './pages/ResetPassword';
-import SeatLayout from './pages/_SeatLayout';
-import { Navbar } from './pages/_NavigationBar';
 import RequestOtp from './pages/RequestOtp';
 import { LandingPage } from './pages/LandingPage';
-import CSVUpload from './components/ui/_CSVUpload';
-import FloorPlan from './components/ui/FloorPlan';
 import { HamburgerMenu } from './pages/HamburgerMenu'
-import { MainPage } from './pages/_MainPage';
 import { MyBookings } from './components/LeftNavigation/MyBookings';
 import { ContactUsPage } from './pages/ContactUs';
-// import { ConfirmationPage } from './components/ui/Confirmation';
-// import { TestPage } from './components/Testpage;
 
 function AppContent() {
-    const location = useLocation();
-    const excludeNavbarPaths = ['/login', '/register', '/resetpassword', '/request-otp', '/reset-password', '/', '/main', '/hamburger', '/mybookings', 'testpage', '/confirmation', '/contact-us'];
 
     return (
         <>
-            {!excludeNavbarPaths.includes(location.pathname) && <Navbar />}
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/resetpassword" element={<ResetPassword />} />
-                <Route path="/seatlayout" element={<SeatLayout />} />
                 <Route path="/request-otp" element={<RequestOtp />} />
                 <Route path='/reset-password' element={<ResetPassword />} />
                 <Route path='/' element={<LandingPage />} />
-                <Route path='/csv' element={<CSVUpload />} />
-                <Route path='/floor' element={<FloorPlan />} />
-                <Route path='/main' element={<MainPage />} />
                 <Route path='/hamburger' element={<HamburgerMenu />} />
                 <Route path='/mybookings' element={<MyBookings />} />
-                <Route path='/contact-us' element={<ContactUsPage />} />
-                {/* <Route path='/confirmation' element={<ConfirmationPage/>}/> */}
-                {/* <Route path='/testpage' element={<TestPage/>}/> */}
+                <Route path='/contact-us' element={<ContactUsPage />} />   
             </Routes>
         </>
     );
